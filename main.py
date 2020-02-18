cfaa92e4eccfabbe62dbb4a6517e052d3e1a72e0
alex <alex.sd3@gmail.com>
2020-02-19 00:33:51 +0100

    halving

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from config import token
 from commands.countdown import countdown
 from commands.registernode import registernode
 from commands.ticker import ticker
+from commands.halving import halving
 from util import sqlite_init
 
 import logging
@@ -32,5 +33,7 @@ if __name__ == '__main__':
     blockHeight_handler = CommandHandler('price', ticker)
     dispatcher.add_handler(blockHeight_handler)
 
+    halving_handler = CommandHandler('halving', halving)
+    dispatcher.add_handler(halving_handler)
 
     updater.start_polling()
